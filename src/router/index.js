25a2cf67563adaf1cb66dd29e7fe09f3b3001d80
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-02-07 14:23:20 +0100

    Création des routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from "../views/AccueilView.vue"
 import ProjetsView from "../views/ProjetsView.vue"
+import MeConnaitreView from "../views/MeConnaitreView.vue"
+import LegalView from "../views/LegalView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/accueil', name: 'accueil', component: AccueilView},
+    {path: '/', name: 'accueil', component: AccueilView},
     {path: '/projets', name: 'projets', component: ProjetsView},
+    {path: '/legal', name: 'legal', component: LegalView},
+    {path: '/meconnaitre', name: 'meconnaitre', component: MeConnaitreView},
   ]
 })
 
