35e14931fc2d4d4bcdcc8ede023871f4a9825896
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-02-09 13:00:37 +0100

    création du svg flèche

---
+++ src/components/icons/fleche.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg
+  width="49"
+  height="79"
+  viewBox="0 0 49 79"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[70px] h-[40.06px]"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <path
+    d="M24.7634 4.14307L24.7634 68.4724"
+    stroke="#515151"
+    stroke-width="8"
+    stroke-miterlimit="10"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path
+    d="M44.9185 56.1662L25.0162 73.8541"
+    stroke="#515151"
+    stroke-width="8"
+    stroke-miterlimit="10"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path
+    d="M4.86116 56.4552L24.7634 74.1431"
+    stroke="#515151"
+    stroke-width="8"
+    stroke-miterlimit="10"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
