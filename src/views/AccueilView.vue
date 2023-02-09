0722f1661dada89e752b38658af29fa1aa36c587
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-02-09 21:24:19 +0100

    fin intégration page accueil

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -2,16 +2,17 @@
 <!--Header-->
       <div class="flex justify-between items-center">
         <h2 class="bg-[url('/RondNoir.jpg')] bg-contain pl-16 md:26 bg-no-repeat text-rose_pastel font-anton h1">Julie Albini</h2>
-        <div class="xl:hidden">
-          <Header />
+        <div class="xl:hidden pr-5">
+          <Header  />
         </div>
-        <div class="hidden xl:block">
+        <div class="hidden pr-16 xl:block">
           <HeaderOrdi/>
         </div>
       </div>
       
 
 <!--ME DECOUVRIR PROFESSIONNELLEMENT-->
+    <!--ME DECOUVRIR-->
     <div class="flex justify-center items-center  lg:gap-28 md:gap-10">
         <div class="md:hidden">
           <div class="justify center m-5 -gap-36">
@@ -30,6 +31,7 @@
           lg:text-[160px] xl:text-[180px]">DÉCOUVRIR</p>
         </div>
         
+        <!--PROFESSIONNELLEMENT-->
         <div class="mt-10 ">
           <img class="w-1/4 h-3/4 absolute" src="../../public/fond.jpg" alt=""/>
           <div class="flex flex-col items-center relative">
@@ -52,23 +54,25 @@
 
 <!--INTRODUCTION-->
 
-        <div class="mt-28 md:flex justify-center items-center">
+      <!--PHOTO-->
+        <div class="mt-56 md:flex justify-center items-center">
           <div class="lg:flex">
-            <div class="lg:w-1/2">
-              <img class="hidden lg:block" src="../../public/traits/MoyenRose.jpg" alt="ligne de décoration"/>
+            <div class="xl:w-1/4">
+              <img class="hidden xl:block" src="../../public/traits/MoyenRose.jpg" alt="ligne de décoration"/>
             </div >
             <div class="">
-              <img class=" xl:hidden w-full absolute  md:static " src="../../public/photo.jpg" alt="photo Julie Albini"/>
+                <img class=" xl:hidden object-fill w-full absolute  md:static " src="../../public/photo.jpg" alt="photo Julie Albini"/>
               <img class=" hidden xl:block w-full absolute  md:static " src="../../public/photozoom.jpg" alt="photo Julie Albini"/>
             </div>
           </div>
           <div class="">
             <div class="md:flex justify-center">
               <img class="hidden md:block" src="../../public/traits/GrandViolet.jpg" alt="ligne décorative"/>
-              <!--<Trait couleur="#7879F1" hauteur="428px"/>-->
             </div>
+
+      <!--TEXTE VIOLET TRANSPARENT-->
             <div class="lg:flex items-center ">
-              <div class="flex justify-end content-end  md:static relative">
+              <div class="flex justify-end content-end md:static relative top-[333px]">
                   <div class="w-3/4 h-max md:w-content p-3  bg-violet_pastel bg-opacity-50">
                     <p class="font-poppins text-white h3">Bonjour !</p>
                     <p class="font-poppins text-white texte">Découvrez moi professionnellement a travers les projets que j’ai réalisés !</p>
@@ -85,6 +89,7 @@
 
 <!--COMPETENCES-->
 
+      <!--RIGUEUR-->
       <div class="xl:mx-28">
         <div class="mt-96 md:mt-28">
           <img class="w-max md:w-1/3 xl:w-1/3 absolute" src="../../public/TraitTurquoise.jpg" alt=""/>
@@ -93,6 +98,7 @@
           </div> 
         </div>
 
+      <!--TENACITE-->
         <div class="mt-10 flex flex-col items-end">
           <img class="w-max xl:w-1/3 absolute" src="../../public/TraitRose.jpg" alt=""/>
           <div class="flex relative">
@@ -100,6 +106,7 @@
             </div> 
         </div>
 
+      <!--CREATIVITE-->
         <div class="mt-10">
           <img class="w-max xl:w-1/3 absolute" src="../../public/TraitViolet.jpg" alt=""/>
           <div class="flex relative">
@@ -110,33 +117,37 @@
 
 <!--LE CONCEPT-->
 
+      <!--PHOTO-->
       <div class="hidden md:flex justify-center mt-20">
         <div class="img">
           <div class="filtre"></div>
         </div>
       </div>
-      <div class="md:hidden mt-28">
+      <div class="md:hidden flex justify-center mt-10">
         <img class="w-full " src="../../public/photo_coupee_bleu.jpg"/>
       </div>
 
-      <div class="mt-28 flex justify-center">
-        <div class="flex items-center lg:w-1/2 ">
+    <!--DECORATION TRAITS BLEUS-->
+      <div class="mt-10 flex justify-center lg:justify-around xl:justify-center">
+        <div class="lg:flex items-center  xl:w-1/2 ">
           <div class="">
-            <img class="hidden lg:block" src="../../public/traits/GrandBleu.jpg" alt="ligne décorative"/>
+            <img class="hidden xl:block" src="../../public/traits/GrandBleu.jpg" alt="ligne décorative"/>
             <div class="flex justify-center">
-              <img class= "hidden lg:block" src="../../public/traits/PetitBleu.jpg" alt="ligne décorative"/>
+              <img class= "hidden xl:block" src="../../public/traits/PetitBleu.jpg" alt="ligne décorative"/>
             </div>
           </div>
-          <img class="hidden lg:block" src="../../public/traits/MoyenBleu.jpg" alt="ligne décorative"/>
+          <img class="hidden xl:block" src="../../public/traits/MoyenBleu.jpg" alt="ligne décorative"/>
         </div>
-        <div class="flex items-center">
-          <div class="w-1/2 ">
-            <img class="lg:hidden" src="../../public/traits/MoyenBleu.jpg" alt="ligne décorative"/>
+        <div class="flex justify-center md:gap-0 gap-14 items-center">
+          <div class="md:w-1/2">
+            <img class="flex justify-center xl:hidden" src="../../public/traits/MoyenBleu.jpg" alt="ligne décorative"/>
           </div>
-          <div class="flex w-1/2  md:justify-start lg:justify-end items-center ">
+        
+          <!--RECTANGLE ROSE-->
+          <div class="flex w-1/2 md:justify-start lg:justify-end items-center ">
             <div class="w-[320px] p-5 h-[520px] flex flex-col justify-center  bg-rose_pastel bg-opacity-50 ">
               <h4 class="font-bold font-poppins  text-white h3">Le concept</h4>
-              <p class="leading-relaxed xl:leading-normal font-bold texte font-poppins text-Gris_foncé">Ce site web a été créé dans le but 
+              <p class="pb-3 leading-relaxed xl:leading-normal font-semibold texte font-poppins text-Gris_foncé">Ce site web a été créé dans le but 
                 de vous permettre de me découvrir professionnellement, et de me donner l’opportunité 
                 de mettre en avant les compétences que j’ai pu acquérir grâce aux projets que j’ai menés.</p>
             </div>
@@ -146,6 +157,8 @@
       </div>
         
 <!--ME CONNAITRE-->
+
+      <!--PHOTO-->
       <div class="hidden md:flex mt-28 ">
         <div class="imgDessous">
           <div class="filtreRose"></div>
@@ -154,10 +167,9 @@
           </div>
         </div>
       </div>
-
       <div class="">
-        <img class="mt-28 w-full md:hidden" src="../../public/photo_coupee_rose.jpg"/>
-        <div class="flex md:justify-end">
+        <img class="mt-10 w-full md:hidden" src="../../public/photo_coupee_rose.jpg"/>
+        <div class="flex md:justify-end lg:ml-10">
           <div class="flex md:w-max md:py-3 md:px-20 justify-center w-1/2 bg-Turquoise_pastel bg-opacity-40">
             <h3 class="text-[40px] xl:text-[60px] text-white font-anton">Julie</h3>
           </div>
@@ -166,19 +178,70 @@
           </div>
         </div>
 
-        <div class="flex">
-          <div class="w-1/2 h-full bg-Gris_foncé p-5">
+        <!--PARAGRAPHE GRIS-->
+        <div class="flex xl:justify-center justify-around xl:gap-28 md:items-center">
+          <div class="w-1/2 h-full md:flex items-center md:w-[300px] md:h-[200px] lg:h-[300px] bg-Gris_foncé p-5">
             <p class="text-white font-poppins texte">Je suis actuellement étudiante en deuxième 
               année de BUT Métiers du Multimédia et de l’Internet et ai choisi de me spécialiser 
               en développement...</p>
           </div>
-          <div class="w-1/2 h-1/2 py-12 flex justify-center items-center bg-violet_pastel bg-opacity-50">
-              <router-link to="/meconnaitre">
-                <Bouton class=" bg-Rose ">Me connaître</Bouton>
-              </router-link>
+        <!--BOUTON-->
+          <div class="w-1/2 h-1/2 md:flex items-center justify-center">
+            <div class=" py-16 md:w-max px-5 flex justify-center items-center bg-violet_pastel bg-opacity-50">
+                <router-link to="/meconnaitre">
+                  <Bouton class=" bg-Rose ">Me connaître</Bouton>
+                </router-link>
+            </div>
+            <img class="hidden md:block" src="../../public/traits/PetitViolet.jpg" alt="ligne décorative"/>
           </div>
         </div>
       </div>
+
+<!--TELECHARGEMENT DU CV-->
+      <div class="mt-28 bg-Turquoise bg-opacity-40 xl:w-full">
+        <div class="flex justify-center items-center gap-5 md:gap-10">
+          <FlecheRose class="md:w-[82px] md:h-[49px] lg:w-[102px] lg:h-[69px] xl:w-[150px] xl:h-[85px] "/>
+          <p class="text-[50px] lg:text-[60px] xl:text-[80px] font-anton text-Noir">MON CV</p>
+        </div>
+      </div>
+
+<!--MES PROJETS-->  
+
+      <!--FOND VIOLET-->
+      <div class="flex mt-28 justify-end">
+        <img class="md:w-1/2 h-3/4 w-3/4 xl:h-3/4  absolute" src="../../public/FondViolet.jpg" alt=""/>
+      </div>
+
+      <!--LIGNGE DECORATIVE-->
+      <div class="relative mt-10 md:flex justify-center items-center">
+        <div class=" flex items-center justify-end">
+          <img class="hidden xl:block" src="../../public/traits/GrandRose.jpg" alt="Ligne décorative"/>
+          <img class="hidden md:block rotate-90 " src="../../public/traits/GrandBleu.jpg" alt="Ligne décorative"/>
+        </div>
+
+        <!--IMAGE PROJET-->
+        <div class="flex flex-col gap-20">
+          <div>
+            <img class="w-full" src="../../public/MaquetteThe100.jpg" alt="image de la maquette Figma The 100"/>
+          </div>
+        <!--BOUTON-->
+        <div class="flex mr-20 justify-end">
+          <div class="md:flex md:gap-16 lg:gap-28 xl:gap-56 items-center">
+              <p class="md:hidden mb-10 font-anton text-white text-[40px] md:text[50px] lg:text-[60px] 
+              xl:text-[70px]">MES PROJETS</p>
+              <router-link to="/mesprojets">
+                <Bouton class="bg-Gris_foncé ">Découvrir</Bouton>
+              </router-link>
+          <!--TITRE PROJET-->
+              <div class="hidden md:block">
+                  <p class="font-anton text-white h3">MES PROJETS</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer class="mt-36 mb-16"/>
       
 </template>
 
@@ -188,6 +251,8 @@ import HeaderOrdi from "../components/HeaderOrdi.vue";
 import Fleche from "../components/icons/fleche.vue"
 import Trait from "../components/trait.vue"
 import Bouton from "../components/bouton.vue"
+import FlecheRose from "../components/icons/flecheRose.vue"
+import Footer from "../components/Footer.vue"
 
 export default {
   data: function () {
@@ -200,7 +265,9 @@ export default {
     HeaderOrdi,
     Fleche,
     Trait,
-    Bouton
+    Bouton,
+    FlecheRose,
+    Footer
   }
 }
 
