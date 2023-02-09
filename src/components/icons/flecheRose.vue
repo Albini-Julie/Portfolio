8dfb8a62aa258876af99b84c7054b8709321948d
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-02-09 21:24:08 +0100

    Création svg insta, linkedin, mail, fleche rose

---
+++ src/components/icons/flecheRose.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg
+  width="70"
+  height="36"
+  viewBox="0 0 70 36"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[62.35px] h-[29.64px]"
+  preserveAspectRatio="none"
+>
+  <path
+    d="M3.97562 18.2141H61.2792"
+    stroke="#FF8FA3"
+    stroke-width="6"
+    stroke-miterlimit="10"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path
+    d="M50.3171 3.30298L66.0732 18.0271"
+    stroke="#FF8FA3"
+    stroke-width="6"
+    stroke-miterlimit="10"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path
+    d="M50.5745 32.9383L66.3306 18.2141"
+    stroke="#FF8FA3"
+    stroke-width="6"
+    stroke-miterlimit="10"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
