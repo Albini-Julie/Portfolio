cf84acb199cce737fea54d16575463cb99805aa7
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-02-10 17:28:20 +0100

    Intégration du composant ProjetGauche

---
+++ src/components/ProjetGauche.vue
@@ -0,0 +1,117 @@
+<template>
+
+    <!--PHOTO PROJET-->
+    <div class="mt-10">
+        <div class="md:flex items-end justify-around xl:justify-evenly md:mb-10 gap-5">
+            <div class="lg:flex items-center">
+                <img class="md:flex justify-center md:w-[566px] md:h-[312px] mt-20 w-full" :src="image"/>
+                <img class="hidden lg:block rotate-90 opacity-30" :src="trait3" alt="Ligne décorative"/>
+            </div>
+        <!--TYPE-->
+            <div class="hidden md:block w-1/2 h-1/2 md:w-max  ">
+                <div class="flex flex-col">
+                    <div class="bg-violet_pastel md:px-3 py-2 bg-opacity-50 flex justify-center">
+                        <p class="font-anton text-white text-[25px] lg:text-[40px] xl:text-[50px]">{{ type }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!--DESCRIPTION-->
+        
+        <div class="flex md:justify-around xl:justify-evenly md:items-center">
+
+                
+            <!--<div class="xl:flex xl:items-center">-->
+                <div class="w-1/2 h-1/2 lg:w-1/3 flex justify-center xl:w-1/4 ">
+                    <div class="px-3 py-5 xl:py-10 bg-Turquoise bg-opacity-40">
+                        <p class="font-poppins text-[20px] xl:text-[22px] text-white font-semibold">
+                            {{description}}
+                        </p>
+                    </div>
+                </div>
+
+                <!--TYPE-->
+            <div class=" w-1/2 h-1/2 md:w-max flex flex-col ">
+                <div class="flex flex-col">
+                    <div class="md:hidden bg-violet_pastel md:px-3 py-2 bg-opacity-50 flex justify-center">
+                        <p class="font-anton text-white text-[25px] xl:text-[50px]">{{ type }}</p>
+                    </div>
+                </div>
+                
+    <!--BOUTON-->
+            <div class="flex mt-10 justify-center">
+                <router-link to="/mesprojets">
+                    <Bouton class="bg-violet_pastel ">Découvrir</Bouton>
+                </router-link>
+            </div>
+
+    <!--J'AIME-->
+            <div class="mt-5 flex justify-center items-center gap-5">
+                <p class="text-[30px] xl:text-[50px] font-poppins font-bold">{{ jaime }}</p>
+                <Coeur class="w-[46px] h-[38px] md:w-[51px] md:h-[43px] xl:w-[61px] xl:h-[53px]"/>
+            </div>
+            </div>
+                <!--
+                    <div class="">
+                        <img class="hidden xl:block" src="../../public/traits/PetitViolet.jpg" alt="ligne décorative"/>
+                    </div>
+                -->
+            
+    <!--FILIERE LG-->
+                <div class="hidden md:w-max md:h-max md:flex w-1/2 bg-Rose items-center justify-center">
+                    <p class="font-anton text-white text-[31px] xl:text-[50px]">{{filiere}}</p>
+                </div>
+            
+        </div>
+    </div>
+
+    <div class="flex items-center md:hidden">
+    <!--FILIERE-->
+        <div class="w-1/2 bg-Rose flex items-center justify-center">
+            <p class="font-anton text-white text-[31px] xl:text-[50px]">{{filiere}}</p>
+        </div>
+
+    <!--TRAIT-->
+            <div class="ml-10  md-hidden w-max">
+                <img class="" :src="trait2" alt="ligne décorative"/>
+            </div>
+    </div>    
+
+</template>
+
+<script>
+import Bouton from "../components/bouton.vue"
+import Coeur from "../components/icons/coeur.vue"
+
+export default {
+    props:{
+        type: String,
+        image: String,
+        filiere: String,
+        description: String,
+        jaime: String,
+        trait1: String,
+        trait2: String,
+        trait3: String
+    },
+    components:{
+        Bouton,
+        Coeur
+    },
+}
+</script>
+
+<style>
+.filtre {
+  width:100%;
+  height:100%;
+  background:rgba(0, 223, 237, 0.4)
+}
+
+.img {
+  width:550px;
+  height:300px;
+  background:url("../../public/MaquetteThe100.jpg") center;
+  background-repeat: no-repeat;
+}
+</style>
