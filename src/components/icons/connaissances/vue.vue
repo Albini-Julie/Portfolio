7a779814fb3d0672fe0467be732a03df126ad2d1
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-02-20 22:20:16 +0100

    ajout des icones Connaissances

---
+++ src/components/icons/connaissances/vue.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg
+  width="132"
+  height="150"
+  viewBox="0 0 132 150"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[131.25px] h-[150px] relative"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <g clip-path="url(#clip0_179_1117)">
+    <path
+      d="M104.561 18.8379H82.0312L65.625 44.7949L51.5625 18.8379H0L65.625 131.25L131.25 18.8379H104.561ZM16.3184 28.2129H32.0801L65.625 86.2793L99.1406 28.2129H114.902L65.625 112.646L16.3184 28.2129Z"
+      fill="#7879F1"
+    ></path>
+  </g>
+  <defs>
+    <clipPath id="clip0_179_1117"><rect width="131.25" height="150" fill="white"></rect></clipPath>
+  </defs>
+</svg>
+</template>
---
+++ src/components/icons/html.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg
+  width="200"
+  height="200"
+  viewBox="0 0 200 200"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-[200px] h-[200px] relative"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <g clip-path="url(#clip0_179_1071)">
+    <path
+      d="M36.3177 100L78.7725 57.5456H57.5451L15.0903 100L57.5451 142.454H78.7725L36.3177 100ZM142.455 57.5456H121.227L163.682 100L121.227 142.454H142.455L184.909 100L142.455 57.5456ZM105.307 36.3184L78.7725 163.682H94.693L121.227 36.3184H105.307Z"
+      fill="#00DFED"
+    ></path>
+  </g>
+  <defs>
+    <clipPath id="clip0_179_1071"><rect width="200" height="200" fill="white"></rect></clipPath>
+  </defs>
+</svg>
+</template>
