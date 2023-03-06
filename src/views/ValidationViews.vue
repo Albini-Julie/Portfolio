b667ea2193e013015bd556c4ba56848f1a51d464
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-03-06 21:29:14 +0100

    Début intégration page validation

---
+++ src/views/ValidationViews.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="dark:bg-black">
+    
+        <!--HEADER-->
+    <div class="flex justify-between items-center">
+        <h2 class="dark:bg-none bg-[url('/RondNoir.jpg')] bg-contain pl-16 md:26 bg-no-repeat text-rose_pastel font-anton neon 
+        text-[50px] md:text-[100px] lg:text-[110px] xl:text-[70px] 2xl:text-[120px]">VALIDATION</h2>
+        <div class="xl:hidden pr-5">
+          <Header  />
+        </div>
+        <div class="hidden pr-16 xl:block">
+          <HeaderOrdi/>
+        </div>
+    </div>
+    </div>
+
+    <!--FOND ROSE-->
+    <div class="flex ">
+            <img class="absolute w-2/3 h-1/3 mt-20 lg:mt-40" src="../../public/TraitRose.jpg" alt="trait décoratif"/>
+    </div>
+    <!--ICONE ET TEXTE-->
+    <div class="md:flex justify-center mt-20 lg:mt-56 items-center">
+        <div class="md:w-1/3">
+            <Valide class="mx-auto"/>
+        </div>
+        <div class="md:w-1/3 flex flex-col items-center">
+            <p class="relative font-poppins h3 dark:text-white text-center">Votre message à bien été envoyé.</p>
+            <h4 class="relative text-[100px] md:[text-120px] lg:text-[150px] xl:text-[180px] text-Turquoise font-anton">MERCI !</h4>
+        </div>
+    </div>
+
+    <!--FOOTER-->
+    <Footer class="mt-36 mb-16 dark:mb-0"/>
+</template>
+
+<script>
+import Header from "../components/Header.vue"
+import HeaderOrdi from "../components/HeaderOrdi.vue"
+import Footer from "../components/Footer.vue"
+import Valide from "../components/icons/iconsValidation.vue"
+
+export default {
+  data: function () {
+    return {
+      menuOuvert: false,
+    };
+  },
+  components: {
+    Header,
+    HeaderOrdi,
+    Footer,
+    Valide
+  },
+}
+
+
+</script>
